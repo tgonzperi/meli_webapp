df04c0b1a05f4073a0889918c5bd5c2f5e22ac1c
tomas <tgonzperi@gmail.com>
2023-10-22 12:15:47 -0300

    Change of library

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
-const http = require('http');
+const express = require("express");
+
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+  res.end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+
+// const http = require('http');
  
-const hostname = '127.0.0.1';
-const port = 80;
+// const hostname = '127.0.0.1';
+// const port = 80;
 
-console.log(port)
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// console.log(port)
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
