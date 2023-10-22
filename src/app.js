0fb2dbda1369b70d6d4957641ea9855fa918573e
tomas <tgonzperi@gmail.com>
2023-10-22 12:06:31 -0300

    Port change

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const http = require('http');
  
-const hostname = 'meli-webapp.onrender.com';
-const port = 3000;
- 
+const hostname = '127.0.0.1';
+const port = 80;
+
+console.log(port)
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
- 
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
